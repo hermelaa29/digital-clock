e7556459567875d31526879f3f67e0b6fecdc38e
Hermela <hermelaa2919@gmail.com>
2025-07-08 01:03:15 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,32 +1,36 @@
 const hour = document.getElementById("hour");
-const minutes = document.getElementById("minutes");
-const second = document.getElementById("seconds");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
 const ampm = document.getElementById("ampm");
 
-function updateClock(){
-    let h = new Date().getHours()
-    let m = new Date().getMinutes()
-    let s = new Date().getSeconds()
-    let ampm = "AM"
 
-    if(h>12){
-        h= h-12
-        ampm = "PM"
-    }
-
+function getTime(){
+    
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+let ampm = "am";
+   
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
 
-    hour.innerText = h;
-    minutes.innerText = m;
-    second.innerText = s;
-    ampm.innerText = ampm;
+if(h>12){
+    h= h-12;
+    ampm = pm;
 
-
-    setTimeout(()=>{
-        updateClock()
-    },1000)
 }
 
-updateClock()
+hour.innerText  = h;
+minute.innerText = m;
+second.innerText = s;
+ampm.innerText = ampm;
+
+
+setTimeout(()=>{
+getTime()
+},1000)
+
+}
+
+getTime();
